8e8066bbff7fea51d5bfc3cd905bc5deb03c1b87
mtslck <mats.loock@lnu.se>
2017-08-15 13:09:03 +0200

    Update

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 /**
- * Starting  point of the application.
+ * The starting point of the application.
  *
  * @author Mats Loock
- * @version 1.0.1
+ * @version 1.1.0
  */
 
-'use strict';
+'use strict'
 
-let abacus = require('./src/abacus.js');
+const abacus = require('./src/abacus.js')
 
-let sum;
+let sum
 
-sum = abacus.add(2, 5, 19);
-console.log(sum); // 26
+sum = abacus.add(2, 5, 19)
+console.log(sum) // 26
 
-sum = abacus.add(2, '21', 19);
-console.log(sum); // 42
+sum = abacus.add(2, '21', 19)
+console.log(sum) // 42
 
-sum = abacus.add(2.5, '5', '3,5', '1.2'); // OBS! Både decimalkomma(,) och decimalpunkt(.) ska fungera
-console.log(sum); // 12.2
+sum = abacus.add(2.5, '5', '3,5', '1.2') // OBS! Både decimalkomma(,) och decimalpunkt(.) ska fungera
+console.log(sum) // 12.2
 
-sum = abacus.add(3, 8, 5, true);
-console.log(sum); // NaN
+sum = abacus.add(3, 8, 5, true)
+console.log(sum) // NaN
 
-sum = abacus.add(3, 8, 5, 'lorem ipsum');
-console.log(sum); // NaN
+sum = abacus.add(3, 8, 5, 'lorem ipsum')
+console.log(sum) // NaN
